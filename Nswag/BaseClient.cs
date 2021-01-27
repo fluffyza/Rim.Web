1fcfb00f46e26b76f4d83d74629ea9e9ba44bd6e
stevenfirsttech <steven@firsttech.digital>
2021-01-27 23:41:11 +0200

    Add project files.

---
+++ Nswag/BaseClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nswag
+{
+	public class BaseClient
+	{
+		public string BearerToken { get; private set; }
+
+		protected Task<HttpRequestMessage> CreateHttpRequestMessageAsync(CancellationToken cancellationToken)
+		{
+			var msg = new HttpRequestMessage();
+			//msg.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", BearerToken);
+			return Task.FromResult(msg);
+		}
+
+		public void SetBearerToken(string token)
+		{
+			BearerToken = token;
+		}
+
+	}
+}
---
+++ wwwroot/js/slider.js
@@ -0,0 +1,33 @@
+﻿function InitializeSlider() {
+	$('.activate-slider').each(function () {
+		$(this).on('click', function () {
+			$(this).parentsUntil(".container-slider").parent().find('.overlay-slider').css('width', '100%');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('overflow', 'hidden');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('width', '0%');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('min-height', '35vh');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('max-height', '100vh');
+		});
+	});
+
+	$('.de-activate-slider').each(function () {
+		$(this).on('click', function () {
+			$(this).parentsUntil(".container-slider").parent().find('.overlay-slider').css('width', '0%');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('width', '100%');
+			$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('height', 'initial');
+		});
+	});
+}
+
+function ManualCloseSlider(divToActivate) {
+	$("#" + divToActivate).parentsUntil(".container-slider").parent().find('.overlay-slider').css('width', '0%');
+	$("#" + divToActivate).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('width', '100%');
+	$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('height', 'initial');
+}
+
+function ManualOpenSlider(divToActivate) {
+	$("#" + divToActivate).parentsUntil(".container-slider").parent().find('.overlay-slider').css('width', '100%');
+	$("#" + divToActivate).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('width', '0%');
+	$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('overflow', 'hidden');
+	$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('min-height', '35vh');
+	$(this).parentsUntil(".container-slider").parent().find('.container-slider-inner').css('max-height', '100vh');
+}
